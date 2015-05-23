60f07d97bc5db11b12858c4ddb5082ddc8ed7f2b
Polly Hwu <pollyhwu@gmail.com>
2015-05-23 17:06:39 -0400

    cleaned up code, added message history ooption

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ var io = require('socket.io')(http);
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
+/****************
+*
+* DB GOODNESS
+*
+*****************/
+
 // db uri
 var dburi = 'mongodb://'+config.db.user+':'+config.db.pw+'@'+config.db.host+':'+config.db.port+'/'+config.db.db;
 
 // establish connection and pushes message to db
 var pushMessage = function(data) {
-	console.log('pushing message');
 	MongoClient.connect(dburi, function(err, db) {
 		assert.equal(null, err);
 		if (err) {
-			console.loog("unable to connect to db");
+			console.log("unable to connect to db");
 		} else {
-			console.log("Connected to the db");
+			console.log("connected to the db");
 			insertMessage(db, data);
 		}
 	});
@@ -27,26 +32,91 @@ var pushMessage = function(data) {
 		// insert docs into the collection
 		messagesCollection.insert(doc, function(err, result) {
 			assert.equal(err, null);
-			//callback(result);
 			db.close();		// we need to wait for the doc to insert before closing the connection
 		});
 	};
 };
 
+// throw the callback once we get all our messages 
+var getOldMessages = function (name, callback) {
+	MongoClient.connect(dburi, function(err, db) {
+		assert.equal(null, err);
+		if (err) {
+			console.log("unable to connect to db");
+		} else {
+			console.log("connected to the db");
+			getMessages(db, name, callback);
+		}
+		
+	});
+
+	var getMessages = function(db, name, callback) {
+		var messagesCollection = db.collection('messageLogs');
+		// insert docs into the collection
+		messagesCollection.find({room: name}).sort({date: -1}).limit(10).toArray(function(err, result) {
+			assert.equal(err, null);
+			if(typeof callback == "function") 
+	        	callback(result);
+			db.close();		
+		});
+	};
+};
+
+/*
+var authenticateUser = function(db, doc, callback) {
+	var usersCollection = db.collection('users');
+	usersCollection.find().toArray(function(err, docs) {
+		
+	});
+};
+*/
+
+/****************
+*
+* ROUTING GOODNESS
+*
+*****************/
 
 // routes
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/main.js', function(req, res){
+  res.sendFile(__dirname + '/main.js');
+});
 
 
-// chat server
+/****************
+*
+* CHATTING GOODNESS
+*
+*****************/
+
+
 var rooms = [];
 
 io.on('connection', function(socket) {
 	socket.emit('system msg', 'Welcome to chat');
 
 	console.log('user connected');
+
+	/****************
+	*
+	* AUTHENTICATING ...
+	*
+	*****************/
+
+	// authenticate the user
+/*	socket.on('authenticate', function(data) {
+
+	});
+*/
+
+	/****************
+	*
+	* ROOMS MANAGEMENT
+	*
+	*****************/
 
 	socket.on('create room', function(name) {
 		// check to see if room exists
@@ -58,6 +128,7 @@ io.on('connection', function(socket) {
 			}
 		}
 		socket.join(name);
+		console.log("Socket joined");
 		// let the client know they have joined the room
 		socket.emit('system msg', 'You have joined ' + name + '.');
 	}); // a thought .. how do we know when to remove a room ? socket.io automatically destroys the room when the last user has left ...
@@ -67,10 +138,29 @@ io.on('connection', function(socket) {
 		socket.emit('rooms list', rooms);
 	});
 
+	/****************
+	*
+	* SEND MESSAGES
+	*
+	*****************/
+
 	socket.on('message to room', function(data) {
 		socket.to(data.room).emit('convo', data);
 		// push message to db
 		pushMessage(data);
+	});
+
+	/****************
+	*
+	* GETTING OLD MESSAGES
+	*
+	*****************/
+
+	// pull old messages from room (name) and send back to client
+	socket.on('fetch old messages', function(name) {
+		var data = getOldMessages(name, function(data) {
+			socket.emit('res old messages', data);
+		});
 	});
 
 
@@ -80,7 +170,13 @@ io.on('connection', function(socket) {
 		socket.emit('system msg', 'User has disconnected');
 	});
 */
-})
+});
+
+/****************
+*
+* ESTABLISH WEB SERVER
+*
+*****************/
 
 // let's listen
 http.listen(3000, function(){
--- main.js
+++ main.js
@@ -1,11 +1,179 @@
-var socket = io();
-      
-$('form').submit(function() {
-	socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-});
-      
-socket.on('chat message', function(msg) {
-	$('#messages').append($('<li>').text(msg));
+$(function(){
+	// establish connection
+	var socket = io();
+
+	// some handy dandy variables
+	var name = '';
+	var mg = '';
+	var room = '';
+
+	/****************
+	*
+	* AUTHENTICATING ...
+	*
+	*****************/
+
+	// get user name 
+	// TODO authentication
+	$('#login').submit(function() {
+		name = $('#name').val();
+		//pw = $('#name').val();
+		//socket.emit('authenticate', {name: name, pw: pw});
+		
+		// will remove once authentication in place
+		$('#login input').prop('disabled', true);
+	    $('#login button').prop('disabled', true);
+	    $('#options li').removeClass('disabled');
+	    $('#oldmessages li').removeClass('disabled');
+	    socket.emit('get rooms', {});
+	    
+	    return false;
+	});
+
+/*
+	socket.on('authenticate', function(data) {
+		if(login is true) {
+			$('#login input').prop('disabled', true);
+	    	$('#login button').prop('disabled', true);
+	    	$('#options li').removeClass("disabled");
+	    	socket.emit('get rooms', {});
+		}else {
+
+		}
+	});
+	*/
+
+	/****************
+	*
+	* ROOM FUNCTIONS
+	*
+	*****************/
+
+	// create a room based on username, client assumes if you create a room, you're going to join it
+	function createRoom() {
+		socket.emit('create room', name);
+		room = name;
+		$('#options li').addClass('disabled');
+		$('#chat button').prop('disabled', false);
+	};
+	// bind on click
+	$('#createroom').click(function() {
+		createRoom();
+	});
+
+	// show available rooms.... this could be helpful for operators
+	socket.on('rooms list', function(rooms) {
+		if(rooms.length > 0) {
+			$('#joinrooms').find('#room-count').text(rooms.length);
+	    	rooms.forEach(function(room) {
+	    		// add to lists and bind the function
+	        	$('#joinrooms').append($('<li>').append($('<a href="#">').text(room))).click(function() {
+	        		joinRoom(room);
+	        	});
+	    	});
+		}
+	});
+
+	// now we can join rooms
+	function joinRoom(rm) { 
+		// basically same as creating a room, cept we pass a room name rather than using the user's name
+	    socket.emit('create room', rm);
+	    room = rm;
+	    $('#options li').addClass('disabled');
+	    $('#chat button').prop('disabled', false);
+	};
+
+	/****************
+	*
+	* SENDING MESSAGES
+	*
+	*****************/
+	      
+	// we always want to send the message to clients in the same room
+	$('#chat').submit(function() {
+		mg = $('#m').val();
+	    var date = new Date();
+
+	    // this will help keep the db clean and ensure the UI looks OK
+	    if(name == '') {
+	    	name = 'null';
+	    }
+	    if(mg == '') {
+	    	mg = 'null';
+	    }
+
+	    var data = {
+	    	name: name,
+	        mg: mg,
+	        room: room,
+	        date: date
+	    };
+
+	    // this will ensure only sockets connected in the same room get the message
+	    socket.emit('message to room', data);
+	    // go ahead and propagate message to screen since it's from your client anyways!
+	    var messageHtml = '<li>' + mg + ' (<span>'+date.toTimeString()+'</span>)</li>';
+	    var nameHtml = '<li><strong>'+ name +'</strong></li>';
+	    $('#messages').append(messageHtml);
+	    $('#names').append(nameHtml);
+	    // reset the message boxx
+	    $('#m').val('');
+	    return false;
+	});
+
+	/****************
+	*
+	* RECEIVING MESSAGES
+	*
+	*****************/
+
+	socket.on('convo', function(data) {
+		var date = new Date(data.date);
+	   	// we got our packet from the other client(s) in the room, let's propagate that data!
+	   	var messageHtml = '<li>' + data.mg + ' (<span>'+date.toTimeString()+'</span>)</li>';
+	    var nameHtml = '<li><strong>'+ data.name +'</strong></li>';
+	    $('#messages').append(messageHtml);
+	    $('#names').append(nameHtml);
+	});
+
+	/****************
+	*
+	* HANDLE SERVER MESSAGES
+	*
+	*****************/
+
+	// messages pushed from the server, can be used to send alerts from server side ...
+	socket.on('system msg', function(msg) {
+		$('#names').append($('<li class="text-uppercase">').text('*****'));
+		$('#messages').append($('<li>').text(msg));
+	});
+
+	/****************
+	*
+	* GETTING OLD MESSAGES
+	*
+	*****************/
+
+	// fetch old messages
+	function fetchOldMgs(name) {
+		socket.emit('fetch old messages', name);
+		$('#oldmessages li').addClass('disabled');
+	};
+	$('#oldmessages').find('a').click(function() {
+		fetchOldMgs(name);
+	});
+
+	socket.on('res old messages', function(data) {
+		if(data.length == 0) {
+			var html = '<li>No messages found :(</li>';
+			$('#oldmessages').append(html);
+		} else {
+			data.forEach(function(msg) {
+				var date = new Date(msg.date);
+				var oldHtml = '<li><em>'+ date.toString() + '</em>   <strong>' + msg.name + ' said </strong>' + msg.mg + '</li>';
+				$('#oldmessages').append(oldHtml);
+			});
+		}
+	});
+
 });
